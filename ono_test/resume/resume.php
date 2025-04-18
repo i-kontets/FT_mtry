b284023bbf92ed4f54e4c8a8916bd7579dc5624d
ono <2410019@i-seifu.jp>
2025-04-18 23:06:51 +0900

    履歴書の左側完成

---
+++ ono_test/resume/resume.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>resume</title>
+    <link rel="stylesheet" href="css/resume.css">
+</head>
+<body>
+    <main>
+        <div class="r_main_left">
+            <div class="r_photo">
+            </div>
+            <!-- タイトル -->
+            <div class="r_title">
+                <p class="title">履歴書</p>
+            </div>
+            <!-- ここまで -->
+             <!-- 日にち -->
+            <div class="r_day">
+                <p>年月日現在</p>
+            </div>
+            <!-- ここまで -->
+            <!-- 名前 -->
+            <div style="vertical-align:middle;position:absolute;top:26mm;left:21mm;width:88.5mm;height:5.2mm;border-width:0.5mm 0.2mm 0.2mm 0.5mm;border-style:solid solid solid solid"></div>
+            <div style="position:absolute;top:25mm;left:22mm;width:88.5mm;height:5.2mm">
+                <font class="furi">
+                    ふりがな
+                </font>
+            </div>
+            <div style="position:absolute;top:25mm;left:21mm;width:88.5mm;height:5.2mm;text-align:center">
+                <font class="furix3">
+                    a
+                </font>
+            </div>
+            <!-- 性別 -->
+            <div style="position:absolute;top:26mm;left:109mm;width:12mm;height:5.2mm;border-width:0.5mm 0.5mm 0.2mm 0.2mm;border-style:solid solid solid solid"></div>
+            <div style="position:absolute;top:25mm;left:109.5mm;width:12mm;height:5.2mm">
+                <font class="idx">
+                    ※男･女
+                </font>
+            </div>
+            <div style="position:absolute;top:26.6mm;left:111.8mm;width:12mm;height:5mm">
+                <font style="font-size:12pt">
+                    ○
+                </font>
+            </div>
+            <!-- 名前 -->
+            <div style="position:absolute;top:31mm;left:21mm;width:88.5mm;height:16.5mm;border-width:0.2mm 0.2mm 0.2mm 0.5mm;border-style:solid solid solid solid"></div>
+            <div style="position:absolute;top:32mm;left:22mm;width:88.5mm;height:16.5mm">
+                <font class="idx">
+                    氏　　名
+                </font>
+            </div>
+            <div style="position:absolute;top:36mm;left:21mm;width:88.5mm;height:16.5mm;text-align:center">
+                <font class="shimei">
+                    a
+                </font>
+            </div>
+            <!-- 印鑑 -->
+            <div style="position:absolute;top:31mm;left:109mm;width:12mm;height:16.5mm;border-width:0.2mm 0.5mm 0.2mm 0.2mm;border-style:solid solid solid solid"></div>
+            <div style="position:absolute;top:32mm;left:110mm;width:12mm;height:16.5mm">
+                <font class="furi">
+                    印
+                </font>
+            </div>
+            <!-- 生年月日 -->
+            <div style="position:absolute;top:47mm;left:21mm;width:100mm;height:10mm;border-width:0.2mm 0.5mm 0.2mm 0.5mm;border-style:solid solid solid solid"></div>
+            <div style="position:absolute;top:51mm;left:22mm;width:100mm;height:10mm">
+                <font class="idx">
+                    生年月日　
+                </font>
+                &nbsp;
+                <font class="idx">
+                    平成　
+                </font>
+                <font class="furix">
+                    50
+                </font>
+                <font class="idx">
+                    　年　　
+                </font>
+                <font class="furix">
+                    &nbsp;1
+                </font>
+                <font class="idx">
+                    　月　
+                </font>
+                <font class="furix">
+                    &nbsp;1
+                </font>
+                <font class="idx">
+                    　日生 (満 
+                </font>
+                <font class="furix">
+                </font>
+                <font class="idx">
+                     才)
+                </font>
+            </div>
+            <!-- 住所 -->
+            <div style="position:absolute;top:57mm;left:21mm;width:100mm;height:5mm;border-width:0.2mm 0.2mm 0.2mm 0.5mm;border-style:solid none none solid"></div>
+            <div style="position:absolute;top:55.5mm;left:22mm;width:100mm;height:5mm">
+                <font class="furi">
+                    ふりがな
+                </font>
+            </div>
+            <div style="position:absolute;top:55.5mm;left:40mm;width:100mm;height:5mm">
+                <font class="furix2">
+                    a
+                </font>
+            </div>
+            <div style="position:absolute;top:62mm;left:21mm;width:120mm;height:11.5mm;border-width:0.2mm 0.2mm 0.2mm 0.5mm;border-style:solid none solid solid"></div>
+            <div style="position:absolute;top:60mm;left:22mm;width:120mm;height:11.5mm">
+                <font class="idx">
+                    現住所　〒
+                </font>
+                <font class="furix">
+                    a
+                </font>
+            </div>
+            <div style="position:absolute;top:67.5mm;left:23mm;width:120mm;height:11.5mm">
+                <font class="adr">
+                    a
+                </font>
+            </div>
+            <!-- 電話番号 -->
+            <div style="position:absolute;top:56.7mm;left:120.4mm;width:52.6mm;height:5mm;border-width:0.5mm 0.2mm 0.2mm 0.2mm;border-style:solid solid none none"></div>
+            <div style="position:absolute;top:56.7mm;left:141mm;width:32mm;height:5.5mm;border-width:0.5mm 0.5mm 0.2mm 0.2mm;border-style:solid solid solid solid"></div>
+            <div style="position:absolute;top:56mm;left:142mm;width:32mm;height:5.5mm">
+                <font class="idx">
+                    電話市外(　　　　)
+                </font>
+            </div>
+            <div style="position:absolute;top:56mm;left:153mm;width:16.5mm;height:5.55mm;text-align:center">
+                <font class="furix">
+                    a
+                </font>
+            </div>
+            <div style="position:absolute;top:62.0mm;left:141mm;width:32mm;height:5.5mm;border-width:0.2mm 0.5mm 0.2mm 0.2mm;border-style:solid solid solid solid;text-align:center"></div>
+            <div style="position:absolute;top:62mm;left:141mm;width:32mm;height:5.5mm;text-align:center">
+                <font class="furix">
+                    a&nbsp;－&nbsp;a
+                </font>
+            </div>
+            <div style="position:absolute;top:67.3mm;left:141mm;width:32mm;height:6mm;border-width:0.2mm 0.5mm 0.2mm 0.2mm;border-style:solid solid solid solid"></div>
+            <div style="text-align:center;position:absolute;top:67mm;left:141mm;width:32mm;height:6mm">
+                <font class="idx">
+                    (　　　　　　方呼出)
+                </font>
+            </div>
+            <div style="position:absolute;top:67mm;left:137mm;width:22mm;height:5.5mm;text-align:right">
+                <font class="furix_n">
+                    あいうえお
+                </font>
+            </div>
+            <!-- 住所２ -->
+            <div style="position:absolute;top:73.1mm;left:21mm;width:152.2mm;height:5mm;border-width:0.2mm 0.5mm 0.2mm 0.5mm;border-style:solid solid none solid"></div>
+            <div style="position:absolute;top:73mm;left:22mm;width:152.2mm;height:5mm;">
+                <font class="furi">
+                    ふりがな
+                </font>
+            </div>
+            <div style="position:absolute;top:73mm;left:40mm;width:152.2mm;height:5mm;">
+                <font class="furix2">
+                    a
+                </font>
+            </div>
+            <div style="position:absolute;top:78.1mm;left:21mm;width:152.2mm;height:11.5mm;border-width:0.2mm 0.5mm 0.5mm 0.5mm;border-style:solid solid solid solid;text-align:right"></div>
+            <div style="position:absolute;top:77mm;left:21mm;width:152.2mm;height:11.5mm;text-align:right">
+                <font class="idx">
+                    　　　　　　　　　　(現住所以外の連絡を希望する場合のみ記入)&nbsp;
+                </font>
+            </div>
+            <div style="position:absolute;top:77mm;left:22mm;width:152.2mm;height:11.5mm">
+                <font class="idx">
+                    連絡先　〒
+                </font>
+                <font class="furix">
+                    a
+                </font>
+            </div>
+            <div style="position:absolute;top:84mm;left:23mm;width:152mm;height:11.5mm">
+                <font class="adr">
+                    a
+                </font>
+            </div>
+            <div style="vertical-align:middle;text-align:right;position:absolute;top:84.5mm;left:111mm;width:60mm;height:6.5mm;border-width:0.2mm 0.2mm 0.2mm 0.5mm;border-style:none none none none">
+                <font class="furix">
+                    a
+                </font>
+            </div>
+            <!-- 学歴・経歴 -->
+            <div style="text-align:center;vertical-align:middle;position:absolute;top:92mm;left:21mm;width:16.4mm;height:7.1mm;border-width:0.5mm 0.2mm 0.2mm 0.5mm;border-style:solid solid solid solid"></div>
+            <div style="text-align:center;position:absolute;top:94mm;left:21mm;width:16.4mm;height:7.1mm">
+                <font class="idx">
+                    年
+                </font>
+            </div>
+            <div style="text-align:center;vertical-align:middle;position:absolute;top:92mm;left:37mm;width:8.2mm;height:7.1mm;border-width:0.5mm 0.2mm 0.2mm 0.2mm;border-style:solid solid solid solid"></div>
+            <div style="text-align:center;position:absolute;top:94mm;left:37mm;width:8.2mm;height:7.1mm">
+                <font class="idx">
+                    月
+                </font>
+            </div>
+            <div style="text-align:center;vertical-align:middle;position:absolute;top:92mm;left:45mm;width:128mm;height:7.1mm;border-width:0.5mm 0.5mm 0.2mm 0.2mm;border-style:solid solid solid solid"></div>
+            <div style="text-align:center;vertical-align:middle;position:absolute;top:94mm;left:45mm;width:128mm;height:7.1mm">
+                <font class="idx">
+                    学歴・職歴(各別まとめて書く)
+                </font>
+            </div>
+            <?php
+            
+            $data=99;
+            for ($i=0; $i < 15; $i++) { 
+                echo <<< HTML
+                <div style="text-align:center;position:absolute;top:{$data}mm;left:21mm;width:16.4mm;height:9.2mm;border-width:0.2mm 0.2mm 0.2mm 0.5mm;border-style:solid solid solid solid"></div>
+                <div style="text-align:center;vertical-align:middle;position:absolute;top:{$data}mm;left:37mm;width:8.2mm;height:9.2mm;border-width:0.2mm 0.2mm 0.2mm 0.2mm;border-style:solid solid solid solid"></div>
+                <div style="text-align:center;vertical-align:middle;position:absolute;top:{$data}mm;left:45mm;width:128mm;height:9.2mm;border-width:0.2mm 0.5mm 0.2mm 0.2mm;border-style:solid solid solid solid"></div>
+                HTML;
+                $data+=9;
+            }
+            ?>
+            <div style="position:absolute;top:99.8mm;left:46mm;width:125.8mm;height:143mm;">
+                <font class="textlist">
+                    a<br> <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                </font>
+            </div>
+            <div style="position:absolute;top:99.8mm;left:22mm;width:20mm;height:143mm;z-index:-1">
+                <font class="textlist">
+                    23<br> <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                </font>
+            </div>
+            <div style="text-align:center;position:absolute;top:99.8mm;left:37.5mm;width:7.2mm;height:143mm;z-index:-1">
+                <font class="textlist">
+                    3<br> <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                </font>
+            </div>
+            <div style="text-align:center;position:absolute;top:234mm;left:21mm;width:16.4mm;height:9.2mm;border-width:0.2mm 0.2mm 0.5mm 0.5mm;border-style:solid solid solid solid"></div>
+            <div style="text-align:center;vertical-align:middle;position:absolute;top:234mm;left:37mm;width:8.2mm;height:9.2mm;border-width:0.2mm 0.2mm 0.5mm 0.2mm;border-style:solid solid solid solid"></div>
+            <div style="text-align:center;vertical-align:middle;position:absolute;top:234mm;left:45mm;width:128mm;height:9.2mm;border-width:0.2mm 0.5mm 0.5mm 0.2mm;border-style:solid solid solid solid"></div>
+            <div style="position:absolute;top:243.1mm;left:21mm;width:125mm;height:8mm">
+                <font class="pic">
+                    記入注意　　数字はアラビア数字で、半角２文字または全角１文字で収める
+                </font>
+            </div>
+
+        </div>
+        <div class="r_main_right">
+        </div>
+    </main>
+</body>
+</html>
